497ddf01744ece4760ae2b3ff2f37e773ab76581
Nguyen Pham <baonguyenyam@gmail.com>
2021-07-02 15:57:41 -0500

    upload

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1726,14 +1726,14 @@ LIFTReady(() => {
 console.log('%cNGUYEN', 'font-size:100px;color:#ff451a;text-shadow:0 1px 0 #f73936,0 2px 0 #f73936 ,0 3px 0 #f73936 ,0 4px 0 #f73936 ,0 5px 0 #f73936 ,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);')
 console.log('%c NGUYEN ' + '%c - The best Web Solutions Provider', 'border-radius: 2px; padding: 3px; background: #ff451a; color: #FFF', 'color: #ff451a')
 console.warn("NGUYEN warning: This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a NGUYEN feature or 'hack' someone's account, it is a scam and will give them access to your NGUYEN account.")
-document.onkeyup = function (e) {
-	var e = e || window.event;
-	if (e.altKey && e.ctrlKey && e.shiftKey && e.which == 13) {
-		$('body')
-		alert('NGUYEN PHAM - +(1) 682 203 1334\nEmail: baonguyenyam@gmail.com\nWeb: baonguyenyam.github.io')
-		return false;
-	}
-};
+// document.onkeyup = function (e) {
+// 	var e = e || window.event;
+// 	if (e.altKey && e.ctrlKey && e.shiftKey && e.which == 13) {
+// 		$('body')
+// 		alert('NGUYEN PHAM - +(1) 682 203 1334\nEmail: baonguyenyam@gmail.com\nWeb: baonguyenyam.github.io')
+// 		return false;
+// 	}
+// };
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 })
@@ -1926,6 +1926,12 @@ function closeMenuAnimation_1() {
 
 
 $(document).ready(function () {
+
+	if($('#lang-vn').length) {
+		$('.lift-js-chatbox__body__header-title-chat').text('Liên lạc tại hạ?')
+		$('.lift-js-chatbox__body__footer-copyright a').html('THIẾT KẾ BỞI <span>NGUYEN PHAM</span>')
+	}
+
 	$("#navbarTogglerNguyen").on("click", "a", function () {
 		$('#nguyenTab a[href="#' + $(this).attr("todata") + '"]').tab('show');
 		$('a[data-toggle="tab"]').on('shown.bs.tab', function (event) {
@@ -2001,26 +2007,26 @@ document.addEventListener('contextmenu', onContextMenu, false);
 
 
 // Disable HOT KEY
-// var isCtrl = false;
-// document.onkeyup=function(e){
-//     if(e.keyCode == 17) isCtrl=false;
-// }
-// document.onkeydown=function(e){
-//     if(e.keyCode == 17) isCtrl=true;
-//     if((e.keyCode == 83 && isCtrl == true) || (e.keyCode == 73 && isCtrl == true) || e.keyCode == 123) {
-//         return false;
-//     }
-// }
-// document.addEventListener("keydown", function(e) {
-// 	if (((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 83) || ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 73) || e.keyCode == 123) {
-// 	  e.preventDefault();
-// 	}
-//   }, false);
-// window.addEventListener("keypress", function(event) {
-// 	if (!(event.which == 115 && event.ctrlKey) && !(event.which == 19)) return true
-// 	event.preventDefault()
-// 	return false
-// })
+var isCtrl = false;
+document.onkeyup=function(e){
+    if(e.keyCode == 17) isCtrl=false;
+}
+document.onkeydown=function(e){
+    if(e.keyCode == 17) isCtrl=true;
+    if((e.keyCode == 83 && isCtrl == true) || (e.keyCode == 73 && isCtrl == true) || e.keyCode == 123) {
+        return false;
+    }
+}
+document.addEventListener("keydown", function(e) {
+	if (((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 83) || ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 73) || e.keyCode == 123) {
+	  e.preventDefault();
+	}
+  }, false);
+window.addEventListener("keypress", function(event) {
+	if (!(event.which == 115 && event.ctrlKey) && !(event.which == 19)) return true
+	event.preventDefault()
+	return false
+})
 
 
 function __nEn(c) {
